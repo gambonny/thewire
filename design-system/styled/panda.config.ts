6055a0aef3269e5cc9d0aadb1fb431a6895e4bd8
Jonny Gamba <gambonny@gmail.com>
2025-03-14 15:00:59 -0500

    feat(design-system:styled): setup panda & add @monorepo/preset

--- design-system/styled/panda.config.ts
+++ design-system/styled/panda.config.ts
@@ -1,20 +1,7 @@
-import { defineConfig } from "@pandacss/dev";
+import { defineConfig } from '@pandacss/dev'
 
 export default defineConfig({
-  // Whether to use css reset
-  preflight: true,
-
-  // Where to look for your css declarations
-  include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
-
-  // Files to exclude
-  exclude: [],
-
-  // Useful for theme customization
-  theme: {
-    extend: {},
-  },
-
+  presets: ['@pandacss/preset-panda', '@monorepo/preset'],
   // The output directory for your css system
-  outdir: "dist",
-});
+  outdir: 'dist',
+})
